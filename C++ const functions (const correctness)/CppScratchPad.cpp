b55fdfb3ec413040da2a41ff19df8e1cb5c693d4
Kulpot <kulpotwahu@gmail.com>
2024-02-04 19:51:20 +0800

    ...

--- C++ const functions (const correctness)/CppScratchPad.cpp
+++ C++ const functions (const correctness)/CppScratchPad.cpp
@@ -2,6 +2,9 @@
 using std::cout;
 using std::endl;
 
+//------------------ C++ const functions (const correctness) --------------------------
+//ref link:https://www.youtube.com/watch?v=ehJKvbbe9hk
+
 void main()
 {
 
